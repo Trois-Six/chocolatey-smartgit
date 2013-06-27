a220ea27c60bce4f281064553b6ec2ea2171f186
Seb Nilsson <seb.nilsson@gmail.com>
2013-06-27 14:14:13 +0200

    Updated SmartGit to 4.5.6

--- SmartGit/tools/chocolateyInstall.ps1
+++ SmartGit/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'SmartGit'
-$url = 'http://www.syntevo.com/download/smartgithg/smartgithg-win32-setup-nojre-4_5_5.zip'
+$url = 'http://www.syntevo.com/download/smartgithg/smartgithg-win32-setup-nojre-4_5_6.zip'
 $silentArgs = '/sp- /silent /norestart'
 
 try {
