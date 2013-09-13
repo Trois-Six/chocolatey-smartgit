97fd99c845fbb0b933f59b543bdba8ea96dbf2e1
Seb Nilsson <seb.nilsson@gmail.com>
2013-09-13 09:51:27 +0200

    Added RavenDB-package

---
+++ RavenDB/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'RavenDB'
+$url = 'http://hibernatingrhinos.com/downloads/RavenDB%20Installer/2700'
+
+try {
+	# $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+	# $downloadPath = Join-Path $toolsDir "raven-install.exe"
+
+	Install-ChocolateyPackage $packageName "exe" "" $url
+
+	# Get-ChocolateyWebFile $packageName $downloadPath $url
+
+    # Install-ChocolateyInstallPackage $packageName "exe" "" $downloadPath
+
+    # Remove-Item $downloadPath
+        
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+    throw
+}
